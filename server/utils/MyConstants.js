6ad286d3438237947ebc32c36724a99510fc2c16
dangducanhdeptrai <128887531+dangducanhdeptrai@users.noreply.github.com>
2024-07-16 22:31:52 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'provjp123.xzsfr86.mongodb.net',
+  DB_USER: 'ducanh2003',
+  DB_PASS: 'Abc123',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'caidicungduoc',
+  JWT_EXPIRES: '360000000', // in milliseconds
+  EMAIL_USER: 'jokermakethestrange@gmail.com', // gmail service
+  EMAIL_PASS: 'crtm xazd ousi slxv'
+};
+module.exports = MyConstants;
